711ceef068697fadafa158cb94db742c0c169e87
Diego Silva <coxcorp@gmail.com>
2022-07-12 16:20:47 -0300

    requisito 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "caneca",
+        "Schimidt",
+        "2022-06-12",
+        "indeterminada",
+        "1234",
+        "Cuidado frágil",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "caneca"
+    assert product.nome_da_empresa == "Schimidt"
+    assert product.data_de_fabricacao == "2022-06-12"
+    assert product.data_de_validade == "indeterminada"
+    assert product.numero_de_serie == "1234"
+    assert product.instrucoes_de_armazenamento == "Cuidado frágil"
