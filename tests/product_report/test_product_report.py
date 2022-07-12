b1b2d386a30e9c5d49b1f1f288f9b6c726e17929
Diego Silva <coxcorp@gmail.com>
2022-07-12 16:52:57 -0300

    requisito 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "caneca",
+        "Schimidt",
+        "2022-06-12",
+        "indeterminada",
+        "1234",
+        "Cuidado frágil",
+    )
+
+    assert str(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
